526b8e5792e9e7477dab4a9613490fb024db5a45
Zeamanual Feleke <zeamanualfeleke@gmail.com>
2022-03-08 11:44:30 +0200

    the front end part of home page and job lists page are completed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let express = require('express')
+require('dotenv').config()
 let app = express()
 let cors = require('cors')
 
 app.use(cors({
 origin:'*'
 }))
+app.use(express.static('./public'))
+app.set('view engine','ejs')
+app.set('views','./views')
 
-app.listen(7000,console.log('server started and listening on port 7000'))
+app.get('/home',(req,res)=>{
+    res.status(200).render("index")
+})
+app.get('/job/all',(req,res)=>{
+    res.status(200).render("job_listing")
+})
+
+let port = process.env.port
+app.listen(port,console.log(`server started and listening on port ${port}`))
