281a2c97e5a0d927d58c067d12a0ee591c15a827
Zeamanual Feleke <zeamanualfeleke@gmail.com>
2022-03-09 08:37:48 +0200

    login and signup page files has been added

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ app.use(express.static('./public'))
 app.set('view engine','ejs')
 app.set('views','./views')
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.status(200).render("index")
+})
+app.get('/employee',(req,res)=>{
+    res.status(200).render("employeeRegister")
+})
+app.get('/login',(req,res)=>{
+    res.status(200).render("login")
+})
+app.get('/detail',(req,res)=>{
+    res.status(200).render("job_details")
+})
+app.get('/employeer',(req,res)=>{
+    res.status(200).render("employeerRegister")
 })
 app.get('/job/all',(req,res)=>{
     res.status(200).render("job_listing")
